21f45990881694ed9f46632880696d3ece4e888a
Felix Nagel <info@felixnagel.com>
2020-06-12 01:09:43 +0200

    Initial commit with basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TER upload test',
+    'description' => 'Dummy extension for testing automatized TER deployments.',
+    'category' => 'example',
+    'author' => 'Felix Nagel',
+    'author_email' => 'info@felixnagel.com',
+    'author_company' => '',
+    'state' => 'alpha',
+    'uploadfolder' => '0',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
